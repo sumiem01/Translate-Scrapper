51b130eaa599318d386925d056c1991578646fe6
sumiem <msumiec@gmail.com>
2020-07-03 21:57:38 +0200

    Change conception of getting definitions. Repair encoding error.

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-import typing as t
 import requests
 from bs4 import BeautifulSoup
 
 
-def get_definitions_from_web(word: str, url: str = "https://dictionary.cambridge.org/dictionary/english/") -> str:
+def get_definitions_from_web(word: str, url: str = "https://dictionary.cambridge.org/dictionary/english/") -> list:
     """
     Cambridge Dictionary Scrapper
     """
     page_html: str = requests.get(url + word).text
     soup = BeautifulSoup(page_html, "html.parser")  # type: bs4.BeautifulSoup
-
+    definitions: list = []
     counter: int = 0
-    back: str = ""
     # "def ddef_d db" I checked looking up source in web browser
-    for i, j in enumerate(soup.find_all("div", class_="def ddef_d db")):
+    for i, j in enumerate(soup.find_all("div", class_="def ddef_d db"), 1):
+        # it is set to 3 because if there were more def then flashcard would be too much
         if counter < 3:
-            back += f"<div><br>{i+1}. {j.text}</div>\n"
+            definition = f"{i}. {j.text}".encode(encoding="utf-8", errors="replace")
+            definitions.append(definition)
             counter += 1
         else:
             break
-    return back
+    return definitions
 
 
 def print_definitions(word: str) -> None:
     print(word,
-          get_definitions_from_web(word).replace("<div><br>", "")
-                                        .replace("</div>", ""),
+          get_definitions_from_web(word),
           sep="\n")
 
 
@@ -34,10 +33,12 @@ def save_definitions_to_file(word: str) -> None:
         list_of_words.write(f'\n{word}\t\"{get_definitions_from_web(word)}\"')
 
 
-def save_definition_interface(word: str) -> None:
+def save_definitions_interface(word: str) -> None:
     if word == "0":
         return None
-    save_decision = input("Would you like to save (Y/N)?")
+    question = "Would you like to save (Y/N)?"
+    print("#" * len(question))
+    save_decision = input(question)
     if save_decision == "Y":
         save_definitions_to_file(word)
 
@@ -47,4 +48,4 @@ if __name__ == "__main__":
     while word != "0":
         word: str = input("Search english word (press 0 to exit): ").lower()
         print_definitions(word)
-        save_definition_interface(word)
+        save_definitions_interface(word)
